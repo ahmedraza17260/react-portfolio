b20f698c743513893aa374ed72404949b22c5c5e
ahmedraza17260 <ahmedraza17260gmail.com>
2020-09-08 13:31:24 +0500

    update footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -32,10 +32,12 @@ class Footer extends Component {
         </div>
         <div className="copyRight">
           <h4> Powered by Ahmed Raza </h4>
-          <a href="https://github.com/ahmedraza17260" rel="noopener noreferrer" target="_blank" >
-            <h3 > Copyright &copy; 2020 Ahmed Raza </h3>
+          <a href="https://github.com/ahmedraza17260" rel="noopener noreferrer" target="_blank">
+            {" "}
+            <h3> Copyright &copy; 2020 Ahmed Raza </h3>{" "}
           </a>
-          <h3 > All Right Reserved </h3></div>
+          <h3> All Right Reserved </h3>
+        </div>
       </footer>
     );
   }
