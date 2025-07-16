2ce4220d1c9b7b954bfcfbada1d3a6450dc70ea0
Ahmed Raza <62701326+ahmedraza17260@users.noreply.github.com>
2025-07-16 17:48:19 +0000

    Update

--- src/Components/Testimonials.js
+++ src/Components/Testimonials.js
@@ -3,6 +3,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+
 class Testimonials extends Component {
   render() {
     const { testimonials } = this.props.data || {};
